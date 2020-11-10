c27d58aeeb130cf1b330dc6efd92317899d3b7de
Yuisdrawing <yuisdrawing@gmail.com>
2020-11-10 00:56:48 -0500

    added mockup assests and created scss

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// Kitty Kustomizer
+
+// button under the heading1 that scrolls down to the customizing section (or to the other page)
+// button click event
+
+// a cat's head image as a base, class of .catHead
+
+// store each cat components:
+    // - fur
+    // - eyes
+    // - nose
+    // - mouth
+
+// make array of cat components?
+// or just objects?
+    // const eyes {
+    //     blue: "link",
+    //     "gold",
+    //     "green"
+    // }
+
+// $('.catEyes').on('change', function() {
+// })
+
+// have default images set up in html
+
+// look up drop down click event
+
+// The array (or objects) of options in a drop-down menu
+// images change on click, depending on what kind of option you choose
+
+// STRETCH GOALS:
+    // Reset button
+    // Export the image as PNG file and trigger save
