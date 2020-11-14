0a83b92f63dc5ef0e008edfe6ff8800643a15618
Yuisdrawing <yuisdrawing@gmail.com>
2020-11-14 13:33:14 -0500

    pushing

--- scripts.js
+++ scripts.js
@@ -1,61 +1,43 @@
-// Kitty Kustomizer
-
-// button under the heading1 that scrolls down to the customizing section (or to the other page)
-// button click event
-
-// a cat's head image as a base, class of .catHead
-
-// store each cat components:
-    // - fur
-    // - eyes
-    // - nose
-    // - mouth
-
-// make array of cat components?
-// or just objects?
-    // const eyes {
-    //     blue: "link",
-    //     "gold",
-    //     "green"
-    // }
-
-// $('.catEyes').on('change', function() {
-// })
-
-// have default images set up in html
-
-// look up drop down click event
-
-// The array (or objects) of options in a drop-down menu
-// images change on click, depending on what kind of option you choose
-
-// STRETCH GOALS:
-    // Reset button
-    // Export the image as PNG file and trigger save
-
-    /////////////////////////////////////////////////
-
     // setting up objects for all of the assets:
     const cat = {
         fur: {
             white: "assets/catHead.png",
             tabby: "assets/tabby.png",
             calico: "assets/calico.png",
-            tuxedo: "assets/tuxedo.png"
+            tuxedo: "assets/tuxedo.png",
+            ginger: "assets/ginger.png",
+            black: "assets/black.png"
         },
         eyes: {
+            default: "assets/eyesdefault.png",
             blue: "assets/eyesBlue.png",
             gold: "assets/eyesGold.png",
             green: "assets/eyesGreen.png",
-            oddEyes: "assets/eyesGreen.png"
+            oddEyes: "assets/eyesOdd.png"
+        },
+        mouth: {
+            default: "assets/mouthDefault.png",
+            silly: "assets/mouthSilly.png",
+            hungry: "assets/mouthHungry.png",
+            happy: "assets/mouthHappy.png"
+        },
+        hats: {
+            noHat: " ",
+            birthdayHat: "assets/birthdayHat.png",
+            topHat: "assets/topHat.png",
+            propellerHat: "assets/propellerHat.png"
         }
     }
 
     $('#coats').on('change', (e) => {
         $("#catFace").attr("src", cat.fur[e.target.value]);
     });
-
     $("#eyes").on("change", (e) => {
         $("#catEyes").attr("src", cat.eyes[e.target.value]);
     });
-
+    $("#mouth").on("change", (e) => {
+        $("#catMouth").attr("src", cat.mouth[e.target.value]);
+    });
+    $("#hats").on("change", (e) => {
+        $("#catHat").attr("src", cat.hats[e.target.value]);
+    });
